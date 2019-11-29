a15b074550c06a9a8a8696de057a1f59c7c2fe18
Farhana <55168133+farhanarahman2299@users.noreply.github.com>
2019-11-29 23:25:16 +0600

    Add files via upload

---
+++ data.s5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[10],a,n;
+  printf("Enter no.of element\n");
+  scanf("%d",&n);
+  peintf("Enter element\n");
+  for(i=1;i<=n;i++)
+  {
+      printf("\na[%d]=",i);
+      scanf("%d",&a[1]);
+  }
+  printf("Given List\n");
+
+
+
+}
